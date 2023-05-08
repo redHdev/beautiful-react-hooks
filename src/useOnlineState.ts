aa71cfac4dbdacf04777d60d0cdbcd8b4fe1ce53
redHdev <128177691+redHdev@users.noreply.github.com>
2023-05-08 08:36:13 -0400

    Update useOnlineState.ts

--- src/useOnlineState.ts
+++ src/useOnlineState.ts
@@ -1,5 +1,5 @@
 import { useState } from 'react'
-import useGlobalEvent from './useGlobalEvent'
+import useGlobalEvent from './useGlobalEvent'  
 
 /**
  * Uses the [Navigator online API](https://developer.mozilla.org/en-US/docs/Web/API/NavigatorOnLine/onLine) to define
