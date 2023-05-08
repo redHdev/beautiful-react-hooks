7ea6cc108e6ffc6bc1c76b5fa8973ca1ffc6264d
redHdev <128177691+redHdev@users.noreply.github.com>
2023-05-08 04:04:13 -0400

    Update useLocalStorage.ts

--- src/useLocalStorage.ts
+++ src/useLocalStorage.ts
@@ -3,6 +3,6 @@ import createStorageHook from './shared/createStorageHook'
 /**
  * Save a value on local storage
  */
-const useLocalStorage = createStorageHook('local')
+const useLocalStorage = createStorageHook('local')   
 
 export default useLocalStorage
